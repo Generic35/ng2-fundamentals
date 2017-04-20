c4eac9464f298b75ca4dcef0f8624d8e2d95b92f
Lamontagne <elamonta@allegisgroup.com>
2017-04-20 13:52:03 -0400

    Added new events component

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>
+            Upcoming Angular2 Events
+        </h1>
+    </div>
+    `
+})
+export class EventListComponent {
+
+}
