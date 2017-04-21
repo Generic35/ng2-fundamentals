d063244d539b761e3c83763b5ac3a51ec8058a31
Lamontagne <elamonta@allegisgroup.com>
2017-04-21 14:51:32 -0400

    added address and styles for it

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -10,8 +10,18 @@ import { Component, Input } from '@angular/core';
         <div>Date:{{event.date}}</div>
         <div>Time:{{event.time}}</div>
         <div>Price:\${{event.price}}</div>
+        <div>
+            <span>Location: {{event.location.address}}</span>
+            
+            <span class="pad-left">{{event.location.city}}, {{event.location.country}}
+                </span>
+        </div>
     </div>
-    `
+    `,
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 
 export class EventThumbnailComponent {
